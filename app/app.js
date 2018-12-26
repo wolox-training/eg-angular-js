b358a4c8c4b92ad7e3df8485b150afa3bea69a47
Edwin Gomez <edwingomez@MacBook-Pro-9.local>
2018-12-26 14:39:57 -0500

    install angularjs and books dashboard structure

---
+++ app/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// Declare app level module which depends on views, and core components
+angular.module('wBooksApp', [
+  'ngRoute',
+  'booksList',
+]).
+config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+  $locationProvider.hashPrefix('!');
+
+  $routeProvider.
+  when('/dashboard', {
+    template: '<books-list></books-list>'
+  }).
+  otherwise('/dashboard');
+}]);
