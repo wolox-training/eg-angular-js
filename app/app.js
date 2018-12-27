76b8b842e471d867d68d1f881eb84894833ed93f
Edwin Gomez <edwingomez@MacBook-Pro-9.local>
2018-12-27 15:09:03 -0500

    Book detail created and Book item migreted to component

--- app/app.js
+++ app/app.js
@@ -2,16 +2,45 @@
 
 // Declare app level module which depends on views, and core components
 angular.module('wBooksApp', [
-  'ngRoute',
+  'ui.router',
+  'core.book',
+  'bookItem',
   'booksList',
+  'bookDetail',
   'wInput'
 ]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
-
-  $routeProvider.
-  when('/dashboard', {
-    template: '<books-list></books-list>'
-  }).
-  otherwise('/dashboard');
+config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+  // An array of state definitions
+  var states = [
+    { 
+      name: 'booksList', 
+      url: '/books', 
+      component: 'booksList',
+      resolve: {
+        books: function(Books) {
+          return Books.get();
+        }
+      }
+    },
+    { 
+      name: 'bookDetail', 
+      // This state takes a URL parameter called personId
+      url: '/books/{id}', 
+      component: 'bookDetail',
+      // This state defines a 'person' resolve
+      // It delegates to the PeopleService, passing the personId parameter
+      resolve: {
+        book: function(Books, $stateParams) {
+          
+          return Books.get().find(function(book) { 
+            return book.id == $stateParams.id;
+          });
+        }
+      }
+    }
+  ];
+  $urlRouterProvider.otherwise('/books');
+  states.forEach(function(state) {
+    $stateProvider.state(state);
+  });
 }]);
