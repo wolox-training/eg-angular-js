5d6d8b9dcc8ccd336b29275e7c30e0d0a8783fe0
Edwin Gomez <edwingomez@MacBook-Pro-9.local>
2018-12-28 14:01:33 -0500

    Search function to es6 arrow function

--- src/books/books.component.js
+++ src/books/books.component.js
@@ -16,7 +16,7 @@ angular.
         value: "author"
       }];
 
-      ctrl.search = function () {
+      ctrl.search = () => {
         ctrl.filter = {};
         if (ctrl.filterField && ctrl.filterField.length > 0 && ctrl.filterCriteria && ctrl.filterCriteria.trim().length > 0) {
           ctrl.filter[ctrl.filterField] = ctrl.filterCriteria;
