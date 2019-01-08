cb940ce642ce0a672064c2dc9b7d05c79994c2f0
Edwin Gomez <edwingomez@MacBook-Pro-9.local>
2019-01-08 11:38:21 -0500

    Refactor for wolox frontend-bootstrap

---
+++ gulp/tasks/purifycss.js
@@ -0,0 +1,30 @@
+import gulp from 'gulp';
+import purifycss from 'gulp-purifycss';
+import { getConfigKeys } from '../config';
+
+const taskOptions = getConfigKeys();
+const localConfig = {
+  src: './build/css/application.css',
+  vendorSrc: './build/css/vendor.css',
+  files: ['./build/**/*.html', './build/**/*.js'],
+  dest: './build/css'
+};
+const options = {
+  minify: taskOptions.minify,
+  rejected: true
+};
+const vendorOptions = Object.assign({rejected: false}, options);
+
+gulp.task('purifycss', ['purifycss:src', 'purifycss:vendor']);
+
+gulp.task('purifycss:src', () => {
+  return gulp.src(localConfig.src)
+    .pipe(purifycss(localConfig.files, options))
+    .pipe(gulp.dest(localConfig.dest));
+});
+
+gulp.task('purifycss:vendor', () => {
+  return gulp.src(localConfig.vendorSrc)
+    .pipe(purifycss(localConfig.files, vendorOptions))
+    .pipe(gulp.dest(localConfig.dest));
+});
