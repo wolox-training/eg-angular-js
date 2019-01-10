1e84e37fcc9fbb54f24267ba08fe3f1956103a2a
Edwin Gomez <edwingomez@MacBook-Pro-9.local>
2019-01-10 14:52:43 -0500

    Api books and book-detail added

--- src/core/auth/session.service.js
+++ src/core/auth/session.service.js
@@ -1,17 +1,24 @@
 'use strict';
 
-angular.module('core.auth').factory('Session', function () {
+angular.module('core.auth').factory('Session', ['$http', function ($http) {
+  const updateSession = () => {
+    $http.defaults.headers.common.Authorization = this.tokenInfo.access_token;
+  };
+
   const localTokenInfo = localStorage.getItem('tokenInfo');
   const localUser = localStorage.getItem('user');
   if (localTokenInfo) {
     this.tokenInfo = JSON.parse(localTokenInfo);
+    updateSession();
   }
   if (localUser) {
     this.user = JSON.parse(localUser);
   }
+
   this.create = function (tokenInfo) {
     localStorage.setItem('tokenInfo', JSON.stringify(tokenInfo));
     this.tokenInfo = tokenInfo;
+    updateSession();
   };
   this.setUser = (user) => {
     localStorage.setItem('user', JSON.stringify(user));
@@ -25,4 +32,4 @@ angular.module('core.auth').factory('Session', function () {
   };
 
   return this;
-});
+}]);
--- src/core/book/book.service.js
+++ src/core/book/book.service.js
@@ -1,9 +1,34 @@
 'use strict';
 
-angular.module('core.book').factory('Books', ['$http', function ($http) {
+angular.module('core.book').factory('Books', ['$http', 'Environment', function ($http, Environment) {
+  const baseUrlApi = Environment.wBooksApiUrl + 'books';
+  const mapper = (book) => {
+    return {
+      id: book.id,
+      author: book.author,
+      title: book.title,
+      genre: book.genre,
+      publisher: book.publisher,
+      year: book.year,
+      imageUrl: book.image_url,
+      description: book.description
+    };
+  };
+
   return {
     get: () => {
-      return $http.get('assets/books.json', { cache: true }).then(resp => {
+      return $http.get(baseUrlApi).then(resp => {
+        if (resp.data) {
+          return resp.data.map(mapper);
+        }
+        return resp.data;
+      });
+    },
+    information: (id) => {
+      return $http.get(`${baseUrlApi}/${id}`).then(resp => {
+        if (resp.data) {
+          return mapper(resp.data);
+        }
         return resp.data;
       });
     }
