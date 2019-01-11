b145693c3d8cccb1d6cf90942d8d7da6988ef72d
Edwin Gomez <edwingomez@MacBook-Pro-9.local>
2019-01-11 07:05:43 -0500

    Translation in login and bart of books dashboard

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 
 angular.module('wBooksApp', [
   'ui.router',
+  'pascalprecht.translate',
   'core.book',
   'bookItem',
   'booksList',
@@ -10,68 +11,94 @@ angular.module('wBooksApp', [
   'signUp',
   'login',
   'navbar'
-]).config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-  function _skipIfAuthenticated($timeout, $state, Auth) {
-    return new Promise(async (resolve, reject) => {
-      if (await Auth.isAuthenticated()) {
-        $timeout(function () {
-          $state.go('booksList');
-        });
-        reject();
-      } else {
-        resolve();
+]).config(['$stateProvider', '$urlRouterProvider', '$translateProvider',
+  function($stateProvider, $urlRouterProvider, $translateProvider) {
+    var translations = {
+      global: {
+        search: 'Buscar',
+        noDataFound: 'No se han encontrado resultados'
+      },
+      login: {
+        title: 'Iniciar sesión',
+        email: 'Correo electrónico',
+        emailRequired: 'Debe ingresar un correo electrónico',
+        emailInvalid: 'Debe ingresar un email válido',
+        password: 'Contraseña',
+        passwordRequired: 'Debe ingresar una contraseña',
+        passwordInvalid: 'Su contraseña que tenga entre 8 y 52 caracteres, y al menos una letra y un número',
+        loginAction: '@:login.title'
+      },
+      booksList: {
+        selectFilter: 'Seleccionar filtro',
+        search: '@:global.search'
       }
-    });
-  }
-  function _redirectIfNotAuthenticated($timeout, $state, Auth) {
-    return new Promise(async (resolve, reject) => {
-      if (await Auth.isAuthenticated()) {
-        resolve();
-      } else {
-        $timeout(function () {
-          $state.go('login');
-        });
-        reject();
-      }
-    });
-  }
+    };
+    $translateProvider
+    .translations('es', translations)
+    .preferredLanguage('es');
+    $translateProvider.useSanitizeValueStrategy('escape');
 
-  var states = [{
-    name: 'booksList',
-    url: '/books',
-    component: 'booksList',
-    resolve: {
-      redirectIfNotAuthenticated: _redirectIfNotAuthenticated,
-      books: (Books) => {
-        return Books.get();
-      }
+    function _skipIfAuthenticated($timeout, $state, Auth) {
+      return new Promise(async (resolve, reject) => {
+        if (await Auth.isAuthenticated()) {
+          $timeout(function () {
+            $state.go('booksList');
+          });
+          reject();
+        } else {
+          resolve();
+        }
+      });
     }
-  }, {
-    name: 'bookDetail',
-    url: '/books/{id}',
-    component: 'bookDetail',
-    resolve: {
-      redirectIfNotAuthenticated: _redirectIfNotAuthenticated,
-      book: (Books, $stateParams) => {
-        return Books.information(+$stateParams.id);
-      }
+    function _redirectIfNotAuthenticated($timeout, $state, Auth) {
+      return new Promise(async (resolve, reject) => {
+        if (await Auth.isAuthenticated()) {
+          resolve();
+        } else {
+          $timeout(function () {
+            $state.go('login');
+          });
+          reject();
+        }
+      });
     }
-  }, {
-    name: 'signUp',
-    url: '/sign-up',
-    component: 'signUp',
-    resolve: {
-      skipIfAuthenticated: _skipIfAuthenticated
-    }
-  }, {
-    name: 'login',
-    url: '/login',
-    component: 'login',
-    resolve: {
-      skipIfAuthenticated: _skipIfAuthenticated
-    }
-  }];
 
-  $urlRouterProvider.otherwise('/books');
-  states.forEach(state => $stateProvider.state(state));
-}]);
+    var states = [{
+      name: 'booksList',
+      url: '/books',
+      component: 'booksList',
+      resolve: {
+        redirectIfNotAuthenticated: _redirectIfNotAuthenticated,
+        books: (Books) => {
+          return Books.get();
+        }
+      }
+    }, {
+      name: 'bookDetail',
+      url: '/books/{id}',
+      component: 'bookDetail',
+      resolve: {
+        redirectIfNotAuthenticated: _redirectIfNotAuthenticated,
+        book: (Books, $stateParams) => {
+          return Books.information(+$stateParams.id);
+        }
+      }
+    }, {
+      name: 'signUp',
+      url: '/sign-up',
+      component: 'signUp',
+      resolve: {
+        skipIfAuthenticated: _skipIfAuthenticated
+      }
+    }, {
+      name: 'login',
+      url: '/login',
+      component: 'login',
+      resolve: {
+        skipIfAuthenticated: _skipIfAuthenticated
+      }
+    }];
+
+    $urlRouterProvider.otherwise('/books');
+    states.forEach(state => $stateProvider.state(state));
+  }]);
--- vendorJs.js
+++ vendorJs.js
@@ -1,5 +1,6 @@
 module.exports = [
   'angular/angular.js',
   '@uirouter/angularjs/release/angular-ui-router.js',
-  'angular-resource/angular-resource.js'
+  'angular-resource/angular-resource.js',
+  'angular-translate/dist/angular-translate.min.js'
 ];
