d35e46d6d88b80d2513b9ae67fd99179255ba417
Edwin Gomez <edwingomez@MacBook-Pro-9.local>
2019-01-11 08:22:23 -0500

    Remove curly brace from arrow functions

--- src/core/auth/auth.service.js
+++ src/core/auth/auth.service.js
@@ -29,12 +29,8 @@ angular.module('core.auth').factory('Auth', ['$http', 'Environment', 'Session', 
         });
       },
       isAuthenticated: () => {
-        return new Promise(resolve => {
-          resolve(!!Session.user);
-        });
+        return new Promise(resolve => resolve(!!Session.user));
       },
-      signOut: () => {
-        Session.destroy();
-      }
+      signOut: () => Session.destroy()
     };
   }]);
