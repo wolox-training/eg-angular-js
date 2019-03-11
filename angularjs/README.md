0873ce96c3dc24af1c68672de18c50c33d4c1b08
Edwin Gomez <edwingomez@MacBook-Pro-9.local>
2019-03-11 13:44:54 -0500

    Refactor to vue bootstrap

---
+++ angularjs/README.md
@@ -0,0 +1,1 @@
+# eg-angular-js
---
+++ src/components/Example.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  .example
+    p
+      | This is {{name}}.
+</template>
+<script>
+export default {
+  data() {
+    return {
+      name: 'Vue single file component'
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.example {
+  padding-top: 30px;
+  text-align: center;
+}
+</style>
